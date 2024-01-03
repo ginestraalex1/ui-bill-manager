510ddbbd3b6ebb80a75369a958f65971099ed256
Ginestra <ginestraalex1@gmail.com>
2024-01-03 17:26:16 +0100

    sass -> scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ui-bill-manager';
